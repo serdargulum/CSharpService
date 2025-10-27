bfa8e40d7e09e5d556b4b7dc6148c662a20cc590
serdargulum <serdargulum77@gmail.com>
2025-10-27 16:41:20 +0300

    Refactor code

---
+++ Migrations/20251027124258_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSharpService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    BrandId = table.Column<long>(type: "bigint", nullable: true),
+                    ProductCategoryId = table.Column<long>(type: "bigint", nullable: true),
+                    TaxId = table.Column<long>(type: "bigint", nullable: true),
+                    StockType = table.Column<int>(type: "int", nullable: false),
+                    Stock = table.Column<ushort>(type: "smallint unsigned", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    IsPrepackedItem = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    IsActive = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    PackagingPrice = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Calories = table.Column<ushort>(type: "smallint unsigned", nullable: true),
+                    SKU = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DeletedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 This is a part of test project https://github.com/serdargulum/TestService
 
 
-## Deploy
+## Deploy
+- Change mysql connection data in `appsettings.json` file
+- Build service
+- Run service : http://localhost:5230
 
-CSharpService : http://localhost:5230
 ```
 dotnet build  
 cd bin/Debug/net9.0
